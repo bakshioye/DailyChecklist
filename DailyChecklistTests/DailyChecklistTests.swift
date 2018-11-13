8d3ad717e708f61969794632b2fd154e6dbb8bd9
Shubham Bakshi <shubhambakshi@shubhams-imac.local>
2018-11-13 11:17:13 +0530

    Initial Commit

---
+++ DailyChecklistTests/DailyChecklistTests.swift
@@ -0,0 +1,34 @@
+//
+//  DailyChecklistTests.swift
+//  DailyChecklistTests
+//
+//  Created by Shubham Bakshi on 13/11/18.
+//  Copyright © 2018 Shubham Bakshi. All rights reserved.
+//
+
+import XCTest
+@testable import DailyChecklist
+
+class DailyChecklistTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
